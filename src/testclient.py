ba1567834803583bccbb4af038973e39a8341ff1
Dieter Whittingham <dieter.whitt@gmail.com>
2024-01-22 23:15:29 -0500

    socket server and client complete

--- src/testclient.py
+++ src/testclient.py
@@ -18,3 +18,32 @@ ADDR = (SERVER, PORT)
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # connect to the server
 client.connect(ADDR)
+
+# way to send message
+def send(msg_str):
+    # encoding the message with utf-8
+    message = msg_str.encode(FORMAT)
+    # need to send header, so get length
+    msg_length = len(message)
+    # encoding the header as a string
+    send_length = str(msg_length).encode(FORMAT)
+    # need to ensure send_length is 8 bytes long
+    # therefore we need to add empty space
+    # adding byte representations of spaces
+    send_length += b' ' * (HEADER - len(send_length))
+    # test to see what the header looks like
+    # send header
+    client.send(send_length)
+    # send message
+    client.send(message)
+
+# testing
+send('hello world!')
+connected = True
+while connected:
+    x = input('enter a message: ')
+    send(x)
+    if(x == DISCONNECT_MESSAGE):
+        connected = False
+        print('you have been disconnected.')
+        

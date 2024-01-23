21e7f15df8ae427252c34e2f7ce40a2db44d37ec
Dieter Whittingham <dieter.whitt@gmail.com>
2024-01-22 22:43:35 -0500

    server and testclient working

--- src/server.py
+++ src/server.py
@@ -35,14 +35,16 @@ def handle_client(conn, addr):
         # here we're taking the first message which will be the header
         # containing the length of the next message
         msg_length = conn.recv(HEADER).decode(FORMAT)
-        msg_length = int(msg_length)
-        # can now put the msg length for the number of bytes received in the second message
-        msg = conn.recv(msg_length).decode(FORMAT)
-        # check disconnect
-        if msg == DISCONNECT_MESSAGE:
-            # stop while loop
-            connected = False
-        print(f'[{addr}]: {msg}')
+        #ensure that the message has some content, since upon connection a "blank" message is sent
+        if msg_length:
+            msg_length = int(msg_length)
+            # can now put the msg length for the number of bytes received in the second message
+            msg = conn.recv(msg_length).decode(FORMAT)
+            # check disconnect
+            if msg == DISCONNECT_MESSAGE:
+                # stop while loop
+                connected = False
+            print(f'[{addr}]: {msg}')
     # while loop ended: close connection
     conn.close()
 
@@ -58,7 +60,7 @@ def start():
         conn, addr = server.accept()
         # new connection: pass the connection to handle_client in a new thread
         # this thread will run parallel with the server waiting for connections
-        thread = threading.thread(target=handle_client, args=(conn, addr))
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
         thread.start()
         # print number of connections
         print(f'active connections: {threading.activeCount() - 1}')
--- src/testclient.py
+++ src/testclient.py
@@ -5,3 +5,16 @@ socket test client file
 '''
 
 import socket
+
+HEADER = 8
+PORT = 9999
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = '!disconnect'
+# figure this out later
+SERVER = '10.32.120.165'
+ADDR = (SERVER, PORT)
+
+# set up client socket
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# connect to the server
+client.connect(ADDR)
